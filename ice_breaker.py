3d3aa13f4ac068d2ef390c1cdf969aaeeb02048e
Samar Manjeshwar <samarmanjeshwar@Samars-MacBook-Air.local>
2025-08-12 21:56:47 -0400

    LinkedIn scraper + OpenAI prompt for summarization

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,21 +1,31 @@
+import json
 import os
 from dotenv import load_dotenv
 from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
+from third_party.linked_in import scrape_linkedin_profile
+from pprint import PrettyPrinter
+
+pp = PrettyPrinter(indent=4)
 
 if __name__ == "__main__":
     load_dotenv()
-    information = """Biswa Kalyan Rath is an Indian stand-up comedian, writer and YouTuber. He came into prominence through the YouTube comedy series, Pretentious Movie Reviews[3] with fellow comedian Kanan Gill.
-        """
-    summary_template = f"""
-        given the information {information} about a person I want you to create:
+    summary_template = """
+        given the Linkedin information {information} about a person, I want you to create:
         1. A short summary
-        2. four interesting facts about them
+        2. two interesting facts about them
         """
     summary_prompt_template = PromptTemplate(
         input_variables=["information"], template=summary_template
     )
-    llm = ChatOpenAI(openai_api_key=os.environ.get("OPENAI_API_KEY"),temperature=0, model_name="gpt-3.5-turbo")
+    llm = ChatOpenAI(
+        openai_api_key=os.environ.get("OPENAI_API_KEY"),
+        temperature=0,
+        model_name="gpt-3.5-turbo",
+    )
     chain = summary_prompt_template | llm
-    res = chain.invoke(input={"information": information})
-    print(res.content)
+    linkedin_data = scrape_linkedin_profile(
+        url="https://www.linkedin.com/in/eden-marco/"
+    )
+    res = chain.invoke(input={"information": linkedin_data})
+    pp.pprint(res.content)
---
+++ third_party/linked_in.py
@@ -0,0 +1,36 @@
+import json
+import os
+from dotenv import load_dotenv
+import requests
+from pprint import PrettyPrinter 
+
+load_dotenv()
+pp = PrettyPrinter(indent=4)
+
+def scrape_linkedin_profile(url, mock=False):
+    """
+    Scrape information from a LinkedIn profile."""
+    if mock:
+        response = requests.get(
+            "https://raw.githubusercontent.com/samarmanjeshwar/LangChain/main/ice_breaker/mock_linkedin_profile.json"
+        )
+    else:
+        linkedin_endpoint = "https://www.linkedin.com/in/eden-marco/"
+        api_endpoint = "https://api.scrapin.io/enrichment/profile"
+        params = {
+            "apikey": os.environ.get("SCRAPIN_API_KEY"),
+            "linkedInUrl": linkedin_endpoint,
+        }
+        response = requests.get(api_endpoint, params=params, timeout=10)
+        data = response.json().get("person")
+        data = {
+            k: v
+            for k, v in data.items()
+            if v not in ([], "", None) and k not in ["certifications"]
+        }
+        return data
+
+if __name__ == "__main__":
+    scrape_linkedin_profile(
+        url="https://www.linkedin.com/in/eden-marco/"
+    )
